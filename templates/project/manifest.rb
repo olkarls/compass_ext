a3d7e7ecc6825e0db2ade2191c4c18d9116f124d
Ola Karlsson <olkarls@gmail.com>
2010-08-05 08:51:59 +0200

    add code to manifest

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,2 +1,3 @@
 # Make sure you list all the project template files here in the manifest.
 stylesheet 'screen.sass', :media => 'screen, projection'
+stylesheet '_code.sass', :media => 'screen, projection'
